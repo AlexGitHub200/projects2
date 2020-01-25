06374ad00b375c40880f444ec5b45c410703eb25
alex <>
2020-01-25 12:14:29 +0200

    Cluster aware Timers

---
+++ src/java/com/test/timers/TimerAnnotation.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.timers;
+
+import java.util.Date;
+import javax.ejb.LocalBean;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ *
+ * @author alex
+ */
+@Singleton
+@LocalBean
+@Startup
+//automatic timer example
+public class TimerAnnotation {
+     @Schedule(second="*/5", minute="*", hour="*")
+    public void execute() {
+        System.out.println("Executing ...");
+        System.out.println("Execution Time from 2 : " + new Date());
+        System.out.println("____________________________________________");   
+    }
+ 
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Dec 14, 2019, 11:40:01 AM
+    Author     : alex
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World Timers!</h1>
+    </body>
+</html>
